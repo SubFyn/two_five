ee0021cd286d9461b2a96816159f74bb20d990ad
SubFyn <subfyn@gmail.com>
2022-02-17 23:17:30 +0500

    new

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,7 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(const DemoListScreen());
-  Map<String, int> map1 = {'zero': 0, 'one': 1, 'two': 2};
-  print(map1);
-// not specify key-value type
-  Map map2 = {'zero': 0, 'I': 'one', 10: 'X'};
-  print(map2);
-  var map3 = {'zero': 0, 'I': 'one', 10: 'X'};
-  print(map3);
+
 }
 class DemoListScreen extends StatelessWidget {
   const DemoListScreen({Key? key}) : super(key: key);
